9c85d3f75e226db6b615d7961c26258a6d470c3c
Maritza Diaz <maritzadiaz77@gmail.com>
2024-09-08 21:51:02 -0700

    Modified index html. Submit btn works. need to fix back btn again.

--- Assets/js/blog.js
+++ Assets/js/blog.js
@@ -1,10 +1,28 @@
 //get blog article out of localstorage
+const username = localStorage.getItem("username");
+const title = localStorage.getItem("title");
+const content = localStorage.getItem("content");
+//after this code, we need to update the UI..
+    //display them
+    //add event listener to button
 
-//display them
+    displayContent();
 
-   //add event listener to button
+    function displayContent() {
+        const storedUsername = localStorage.getItem("username");
+        const storedTitle = localStorage.getItem("title");
+        const storedContent = localStorage.getItem("content");
+        usernameInput.textContent = storedUsername;
+        titleInput.textContent = storedTitle;
+        contentInput.textContent = storedContent;
+    }
+
+
+
+   //the document is searching for the id of backbtn
    const buttonEl = document.querySelector("#backBtn");
-
+   //it is listening for the click
    buttonEl.addEventListener("click",function(){
+    //the window is redirecting us to the index html
    window.location = "./index.html";
    });
--- Assets/js/form.js
+++ Assets/js/form.js
@@ -1,5 +1,26 @@
-// handle form submission
+//we are getting the values and storing in a variable
+const usernameInput = document.querySelector('#username').value;
+const titleInput = document.querySelector('#title').value;
+const contentInput = document.querySelector('#content').value; 
+
+//set the item
     // store the blog article
-    //redirect to the blog page
+    localStorage.setItem('username',username);//saving to localstorage
+    localStorage.setItem('title',title);
+    localStorage.setItem('content',content);
+ //redirect to the blog page
 
  
+
+
+
+
+ //this piece of code is for the submit button to go to the bloc (html-it works!)
+//the document is searching for the id of submitbtn
+const buttonEl = document.querySelector("#submitBtn");
+
+//it is listening for the click
+buttonEl.addEventListener("click",function(){
+ //the window is redirecting us to the blog html
+window.location = "./blog.html";
+});
